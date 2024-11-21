45c94ee3eba46db7bed786d4fd5eb52fdf29a0b1
MychailoM <142823203+MychailoM@users.noreply.github.com>
2024-11-21 14:14:40 +0200

    time calculator start

---
+++ src/js/card-five.js
@@ -0,0 +1,21 @@
+const inputElement = document.querySelector('.time__calculator_input');
+const buttonElement = document.querySelector('.time__calculator_btn');
+const resultElement = document.querySelector('.time__calculator_text');
+
+// Додаємо обробник події для кнопки
+buttonElement.addEventListener('click', () => {
+const inputValue = inputElement.value;
+
+if (!inputValue || isNaN(inputValue) || inputValue < 0) {
+    resultElement.textContent = 'Будь ласка, введіть коректне число!';
+    return;
+}
+
+const totalMinutes = parseInt(inputValue, 10);
+
+    const days = Math.floor(totalMinutes / (24 * 60));
+    const hours = Math.floor((totalMinutes % (24 * 60)) / 60);
+    const minutes = totalMinutes % 60;
+
+resultElement.textContent = `${days} дн. ${hours} : ${minutes}`;
+});
