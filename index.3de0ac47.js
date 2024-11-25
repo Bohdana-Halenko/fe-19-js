0364cea6c3bde901fa96599d02fc669eef3249e2
Bohdana-Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-11-25 16:58:37 +0000

    Deploying to gh-pages from @ Bohdana-Halenko/fe-19-js@62f3f303e94be631f59d098b5cd84c9e9f758c1c 🚀

---
+++ index.3de0ac47.js
@@ -0,0 +1,2 @@
+const t=document.getElementById("dropdownContent");document.getElementById("dropBtn").addEventListener("clikck",void t.classList.toggle("show"));
+//# sourceMappingURL=index.3de0ac47.js.map
---
+++ index.8cd8bcec.js
@@ -0,0 +1,2 @@
+!function(){var t=document.getElementById("dropdownContent");document.getElementById("dropBtn").addEventListener("clikck",void t.classList.toggle("show"))}();
+//# sourceMappingURL=index.8cd8bcec.js.map
