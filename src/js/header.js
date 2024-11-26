82288103f13abbe4b8ae2fc0151e273ef94cfb76
dzhurar <142822867+dzhurar@users.noreply.github.com>
2024-11-26 15:35:32 +0200

    trying to change theme

--- src/js/header.js
+++ src/js/header.js
@@ -21,3 +21,18 @@ const menuItems = document.querySelectorAll('.header__dropdown-content-item');
 menuItems.forEach(item => {
     item.addEventListener('click', () => {dropdownContent.classList.remove('header__show')});
 });
+// Зміна теми
+const bgContainer = document.querySelectorAll('.container');
+const themeSwitcher = document.getElementById('headerCheckbox');
+
+themeSwitcher.addEventListener('click', () => {
+    bgContainer.forEach((bgContainer) => {
+        if(bgContainer.style.backgroundColor = '#fff'){
+            bgContainer.style.backgroundColor = '#003161'; 
+        } else{
+            bgContainer.style.backgroundColor = '#fff'
+            console.log(bgContainer.style.backgroundColor);
+        }
+
+      });
+});
