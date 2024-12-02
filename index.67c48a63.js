42637fa3b44fac79013213b6567d65b223d3cad1
Bohdana-Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-12-02 17:40:53 +0000

    Deploying to gh-pages from @ dzhurar/fe-19-js@e3a26950fbbd3a20207ebbfbd2c77af196fe1f80 🚀

---
+++ index.67c48a63.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("year__btn"),r=document.querySelector("year__input"),t=document.querySelector("year__resoult");e.addEventListener("click",(()=>{isNaN(r.value)||""===r.value?(t.innerHTML="ви гн ввели чесло",t.style.color="true"===localStorage.getItem("theme")?"#fff":"#000"):(t.innerHTML=r.value%4==0?"вии народились у високосний рік":"ви народились не у високосний рік",t.style.color=r.value%4==0?"red":"green",r.value="")}));
+//# sourceMappingURL=index.67c48a63.js.map
