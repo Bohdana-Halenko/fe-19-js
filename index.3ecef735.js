f10131b9d2d24f6fdb82c782cc56475fb1f94ffd
dzhurar <142822867+dzhurar@users.noreply.github.com>
2024-12-08 16:45:45 +0000

    Deploying to gh-pages from @ dzhurar/fe-19-js@3fba73a7946319d646557de1a8e4ed9255c07883 🚀

--- index.3ecef735.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("year__btn"),r=document.querySelector("year__input"),t=document.querySelector("year__resoult");e.addEventListener("click",(function(){isNaN(r.value)||""===r.value?(t.innerHTML="ви гн ввели чесло",t.style.color="true"===localStorage.getItem("theme")?"#fff":"#000"):(t.innerHTML=r.value%4==0?"вии народились у високосний рік":"ви народились не у високосний рік",t.style.color=r.value%4==0?"red":"green",r.value="")}))}();
-//# sourceMappingURL=index.3ecef735.js.map
