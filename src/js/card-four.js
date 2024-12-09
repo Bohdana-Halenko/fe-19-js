c7f14fffd28130fce63842f62f18e25afe974693
dzhurar <142822867+dzhurar@users.noreply.github.com>
2024-12-09 18:37:19 +0200

    calc unfixed

---
+++ src/js/card-four.js
@@ -0,0 +1,34 @@
+let operation = '';
+
+function setOperation(op) {
+  operation = op;
+}
+
+function calculate() {
+  const num1 = parseFloat(document.getElementById('num1').value);
+  const num2 = parseFloat(document.getElementById('num2').value);
+  let result = '';
+
+  if (isNaN(num1) || isNaN(num2)) {
+    result = 'Помилка';
+  } else {
+    switch (operation) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num2 !== 0 ? num1 / num2 : 'Ділення на 0';
+        break;
+      default:
+        result = 'Оберіть операцію';
+    }
+  }
+
+  document.getElementById('result').value = result;
+}
