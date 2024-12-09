f6852615e2fd67d131a93d98a85ea2d3bad1121a
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-12-09 20:38:57 +0200

    Update card-four.js

--- src/js/card-four.js
+++ src/js/card-four.js
@@ -1,37 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-let operation = ''; 
+  let operation = ''; 
 
-function setOperation(op) {
-  operation = op;
-}
+  window.setOperation = function setOperation(op) {
+    operation = op;
+  };
 
-function calculate() {
-  const num1 = parseFloat(document.getElementById('num1').value);
-  const num2 = parseFloat(document.getElementById('num2').value);
-  let result = '';
+  window.calculate = function calculate() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    let result = '';
 
-  if (isNaN(num1) || isNaN(num2)) {
-    result = 'Помилка: введіть числа';
-  } else {
-
-    switch (operation) {
-      case '+':
-        result = num1 + num2;
-        break;
-      case '-':
-        result = num1 - num2;
-        break;
-      case '*':
-        result = num1 * num2;
-        break;
-      case '/':
-        result = num2 !== 0 ? num1 / num2 : 'Помилка: ділення на 0';
-        break;
-      default:
-        result = 'Помилка: оберіть операцію';
+    if (isNaN(num1) || isNaN(num2)) {
+      result = 'Помилка: введіть числа';
+    } else {
+      switch (operation) {
+        case '+':
+          result = num1 + num2;
+          break;
+        case '-':
+          result = num1 - num2;
+          break;
+        case '*':
+          result = num1 * num2;
+          break;
+        case '/':
+          result = num2 !== 0 ? num1 / num2 : 'Помилка: ділення на 0';
+          break;
+        default:
+          result = 'Помилка: оберіть операцію';
+      }
     }
-  }
 
-  document.getElementById('result').value = result;
-}
+    document.getElementById('result').value = result;
+  };
 });
