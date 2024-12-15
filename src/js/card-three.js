62d54df0775712549b2a6fa975e1048e9777f1fa
dzhurar <142822867+dzhurar@users.noreply.github.com>
2024-12-16 01:16:43 +0200

    Dino fix
    commented David`s sections, fixed dino

--- src/js/card-three.js
+++ src/js/card-three.js
@@ -1,35 +1,35 @@
-let playerScore = 0;
-let computerScore = 0;
+// let playerScore = 0;
+// let computerScore = 0;
 
-function getComputerChoice() {
-  const choices = ["камінь", "ножиці", "папір"];
-  const randomIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomIndex];
-}
+// function getComputerChoice() {
+//   const choices = ["камінь", "ножиці", "папір"];
+//   const randomIndex = Math.floor(Math.random() * choices.length);
+//   return choices[randomIndex];
+// }
 
-function determineWinner(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-    return "Нічия!";
-  }
-  if (
-    (playerChoice === "камінь" && computerChoice === "ножиці") ||
-    (playerChoice === "ножиці" && computerChoice === "папір") ||
-    (playerChoice === "папір" && computerChoice === "камінь")
-  ) {
-    playerScore++;
-    return "Ви виграли раунд!";
-  } else {
-    computerScore++;
-    return "Комп'ютер виграв раунд!";
-  }
-}
+// function determineWinner(playerChoice, computerChoice) {
+//   if (playerChoice === computerChoice) {
+//     return "Нічия!";
+//   }
+//   if (
+//     (playerChoice === "камінь" && computerChoice === "ножиці") ||
+//     (playerChoice === "ножиці" && computerChoice === "папір") ||
+//     (playerChoice === "папір" && computerChoice === "камінь")
+//   ) {
+//     playerScore++;
+//     return "Ви виграли раунд!";
+//   } else {
+//     computerScore++;
+//     return "Комп'ютер виграв раунд!";
+//   }
+// }
 
-function playRound(playerChoice) {
-  const computerChoice = getComputerChoice();
-  const result = determineWinner(playerChoice, computerChoice);
-  const message = `
-    Комп'ютер обрав: ${computerChoice}<br>
-    ${result}<br>
-    Рахунок: Ви - ${playerScore}, Комп'ютер - ${computerScore}
-  `;
-}
+// function playRound(playerChoice) {
+//   const computerChoice = getComputerChoice();
+//   const result = determineWinner(playerChoice, computerChoice);
+//   const message = `
+//     Комп'ютер обрав: ${computerChoice}<br>
+//     ${result}<br>
+//     Рахунок: Ви - ${playerScore}, Комп'ютер - ${computerScore}
+//   `;
+// }
